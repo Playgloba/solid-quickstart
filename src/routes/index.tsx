77807fa29469229aa09911dad7818039a5a41949
Playgloba <82510735+Playgloba@users.noreply.github.com>
2024-04-04 16:12:57 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>PlayGloba - Search nearby Sports Venue, Coaches</Title>
+      <h1>Playgloba: Revolutionizing Sports Engagement with an All-in-One Online Platform</h1>
       <Counter />
       <p>
       In an era where technology seamlessly integrates into every aspect of our lives, the sports industry is no exception. Whether you're a seasoned athlete, a weekend warrior, or just someone who enjoys staying active, finding the right sports venue, coach, or event can often be a daunting task. Enter Playgloba – an innovative online platform designed to streamline the sports experience for enthusiasts of all levels.
